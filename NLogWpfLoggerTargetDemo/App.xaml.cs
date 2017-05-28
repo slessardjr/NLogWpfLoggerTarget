9bb023db341533991306e9200bd914ab31fafabe
Steve Lessard <slessardjr@me.com>
2017-05-28 14:27:17 -0400

    Added Missing Files from Initial Commit. Problem with local .gitIgnore file

---
+++ NLogWpfLoggerTargetDemo/App.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NLogWpfLoggerTargetDemo
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+    }
+}
---
+++ NLogWpfLoggerTargetDemo/NLogWpfLoggerTargetDemo.xaml.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using NLog;
+using LogLevel = NLog.LogLevel;
+
+namespace NLogWpfLoggerTargetDemo
+{
+    /// <summary>
+    /// Interaction logic for NLogWpfLoggerTargetDemo.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        private Task _logTask;
+        private CancellationTokenSource _cancelLogTask;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Send_Click(Object sender, RoutedEventArgs e)
+        {
+            var logLevel = LogLevel.Off;
+
+            if (sender.Equals(ButtonSendInfo)) logLevel = LogLevel.Info;
+            if (sender.Equals(ButtonSendDebug)) logLevel = LogLevel.Debug;
+            if (sender.Equals(ButtonSendTrace)) logLevel = LogLevel.Trace;
+            if (sender.Equals(ButtonSendWarning)) logLevel = LogLevel.Warn;
+            if (sender.Equals(ButtonSendError)) logLevel = LogLevel.Error;
+            if (sender.Equals(ButtonSendFatal)) logLevel = LogLevel.Fatal;
+
+            LogManager.GetLogger("button").Log(logLevel, TextBoxLogTextToSend.Text);
+        }
+
+        private void BackgroundSending_Checked(Object sender, RoutedEventArgs e)
+        {
+            _cancelLogTask = new CancellationTokenSource();
+            var token = _cancelLogTask.Token;
+            _logTask = new Task(SendLogs, token, token);
+            _logTask.Start();
+        }
+
+        private void BackgroundSending_Unchecked(Object sender, RoutedEventArgs e)
+        {
+            _cancelLogTask?.Cancel();
+        }
+
+        private static void SendLogs(Object obj)
+        {
+            var cancellationToken = (CancellationToken)obj;
+
+            var counter = 0;
+            var log = LogManager.GetLogger("task");
+
+            log.Debug("Backgroundtask started.");
+
+            while (!cancellationToken.WaitHandle.WaitOne(2000))
+            {
+                log.Trace("Message # {0} from background task.", counter++);
+            }
+
+            log.Debug("Backgroundtask stopped.");
+        }
+    }
+}
